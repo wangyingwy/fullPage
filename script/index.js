ee3fa932a48e577f34f4db42dd32b0aed697664d
wangying <549884132@qq.com>
2015-05-20 18:17:22 +0800

    update

---
+++ script/index.js
@@ -0,0 +1,11 @@
+$(function() {
+    $("#wrapper").fullpage({
+        continuousVertical: true,
+        //°ó¶¨²Ëµ¥
+        anchors: ['page1', 'page2', 'page3', 'page4','page5','page6'],
+        menu: '#menu',
+        // µ¼º½
+        'navigation': true,
+        resize: true
+    });
+});
